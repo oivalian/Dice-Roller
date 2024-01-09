b928be7cdbed6b246b780fddd2c81cec731f1542
oivalian <109859213+oivalian@users.noreply.github.com>
2024-01-09 11:34:47 +1100

    Created MEIPASS file

---
+++ MEIPASS.py
@@ -0,0 +1,24 @@
+def resource_path(relative_path):
+    try:
+        base_path = sys.MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+# window|root
+root = ttk.Window(themename='darkly')
+iconPath = resource_path('dice.ico')
+root.iconbitmap(iconPath)
+window.title('Dice Roller')
+window.geometry("350x400")
+window.maxsize(350, 400)
+window.minsize(350, 400)
+
+
+'''
+replace lines 25-31 in diceroller.py with the above code
+
+run command:
+pyinstaller -w --onefile --icon=dice.ico --add-data=dice.ico:. diceroller.py
+'''
